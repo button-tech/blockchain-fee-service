bf9366eeb45a72f7a00b3347fc36eed67172b893
Kirill <krboktv@gmail.com>
2019-06-20 04:46:36 -0700

    refactoring

--- services/server/handlers/fee.go
+++ services/server/handlers/fee.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"dev.azure.com/fee-service/dto"
 	"dev.azure.com/fee-service/dto/errors"
+	"dev.azure.com/fee-service/dto/fee/responses"
 	"dev.azure.com/fee-service/services/feeCalculator"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -10,65 +11,59 @@ import (
 
 func GetBitcoinFee(c *gin.Context) {
 	var body dto.GetFeeRequest
+
 	body.ReceiversCount = 1
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetBitcoinFee(body.FromAddress, body.Amount, body.ReceiversCount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
+
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
+
 	c.JSON(http.StatusOK, res)
 }
 
 func GetLitecoinFee(c *gin.Context) {
 	var body dto.GetFeeRequest
+
 	body.ReceiversCount = 1
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetLitecoinFee(body.FromAddress, body.Amount, body.ReceiversCount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
+
 	c.JSON(http.StatusOK, res)
 }
 
 func GetBitcoinCashFee(c *gin.Context) {
 	var body dto.GetFeeRequest
+
 	body.ReceiversCount = 1
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetBitcoinCashFee(body.FromAddress, body.Amount, body.ReceiversCount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
 	c.JSON(http.StatusOK, res)
@@ -76,105 +71,102 @@ func GetBitcoinCashFee(c *gin.Context) {
 
 func GetEthereumFee(c *gin.Context) {
 	var body dto.GetFeeRequest
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetEthereumFee(body.FromAddress, body.Amount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
+
 	c.JSON(http.StatusOK, res)
 }
 
 func GetTokenFee(c *gin.Context) {
 	var body dto.GetTokenFeeRequest
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetTokenFee(body.FromAddress, body.TokenAddress, body.Amount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
+
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
+
 	c.JSON(http.StatusOK, res)
 }
 
 func GetEthereumClassicFee(c *gin.Context) {
 	var body dto.GetFeeRequest
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetEthereumClassicFee(body.FromAddress, body.Amount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
+
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
+
 	c.JSON(http.StatusOK, res)
 }
 
 func GetWavesFee(c *gin.Context) {
 	var body dto.GetFeeRequest
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetWavesFee(body.FromAddress, body.Amount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
+
 	c.JSON(http.StatusOK, res)
 }
 
 func GetStellarFee(c *gin.Context) {
 	var body dto.GetFeeRequest
-	if ok, statusCode, message := handleError(errors.BadRequest{
-		Error:   c.ShouldBindJSON(&body),
-		Message: errors.BadRequestMessage,
-	}); ok != false {
-		c.JSON(statusCode, message)
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, errors.BadRequestMessage)
 		return
 	}
+
 	res, apiErr, err := feeCalculator.GetStellarFee(body.FromAddress, body.Amount)
-	if ok, statusCode, message := handleError(err); ok != false {
-		c.JSON(statusCode, message)
+
+	isErr, statusCode, error := ErrorCheck(err, apiErr)
+	if isErr {
+		c.JSON(statusCode, error)
 		return
 	}
-	if ok, statusCode, message := handleError(apiErr); ok != false {
-		c.JSON(statusCode, message)
-		return
-	}
+
 	c.JSON(http.StatusOK, res)
+}
+
+func ErrorCheck(err error, apiError responses.ResponseError) (bool, int, errors.ApiError) {
+	if ok, statusCode, message := handleError(err); ok {
+		return true, statusCode, message
+	} else if ok, statusCode, message := handleError(apiError); ok {
+		return true, statusCode, message
+	}
+	return false, 0, errors.ApiError{}
 }
