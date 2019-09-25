82fa9a3765cd43d2877af60456c9c7f2d31c2099
Kirill <krboktv@gmail.com>
2019-09-25 19:55:01 +0300

    edit dockerfile

--- build.sh
+++ build.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-if [ -d "./ssh_key/id_rsa" ]
-then
-    echo "Directory ./ssh_key exists."
-else
-    mkdir ./ssh_key
-fi
-
-cat ~/.ssh/sendgrid_rsa > ./ssh_key/id_rsa
-
-chmod 600 ./ssh_key/id_rsa
-
 docker build -t $acc/blockchain-fee-service .
